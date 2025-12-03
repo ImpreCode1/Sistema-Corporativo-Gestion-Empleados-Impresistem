c2c7500c4285ccf0bdea5b719927db3f70062b13
SebastianOG509 <seortizg@unal.edu.co>
2025-12-03 17:15:20 -0500

    Añadir módulo de reclutamiento con servicios, controladores y pruebas; integrar Prisma como ORM

---
+++ svc_reclutamiento/src/reclutamiento/reclutamiento.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { ReclutamientoService } from './reclutamiento.service';
+import { ReclutamientoController } from './reclutamiento.controller';
+
+@Module({
+  providers: [ReclutamientoService],
+  controllers: [ReclutamientoController]
+})
+export class ReclutamientoModule {}
