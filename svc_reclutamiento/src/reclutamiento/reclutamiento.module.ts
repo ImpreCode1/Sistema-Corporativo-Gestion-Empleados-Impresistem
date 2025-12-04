f8fdd895462bd0d4612fe731430846f04d807cf9
SebastianOG509 <seortizg@unal.edu.co>
2025-12-04 10:58:53 -0500

    Añadir importación del módulo Prisma en el módulo de reclutamiento

--- svc_reclutamiento/src/reclutamiento/reclutamiento.module.ts
+++ svc_reclutamiento/src/reclutamiento/reclutamiento.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ReclutamientoService } from './reclutamiento.service';
 import { ReclutamientoController } from './reclutamiento.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [ReclutamientoService],
   controllers: [ReclutamientoController],
 })
